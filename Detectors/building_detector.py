43cb253ef86b44bc27498003e740808b34684f9c
Allyn Bao <baoallyn@gmail.com>
2022-12-04 20:42:01 -0500

    building detector class update

--- Detectors/building_detector.py
+++ Detectors/building_detector.py
@@ -2,7 +2,7 @@ import os
 import detector
 
 
-class Forest_Detector(detector.Detector):
+class Building_Detector(detector.Detector):
     IMAGE_SIZE = 244
     MODEL_PATH = os.path.join('models', 'building_recognition_transf_v1.h5')
     CLASS_NAME = ['building', 'not building']
@@ -15,5 +15,5 @@ class Forest_Detector(detector.Detector):
 
 if __name__ == "__main__":
     sample_images_path = os.path.join("sample_images", "building testing")
-    forest_detector = Forest_Detector()
-    print(forest_detector.predict_images_by_dir_path(sample_images_path))
+    building_detector = Building_Detector()
+    print(building_detector.predict_images_by_dir_path(sample_images_path))
