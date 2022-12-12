0661fe2dac0b0fd1453050409afcc1eb02a3a953
Allyn Bao <baoallyn@gmail.com>
2022-12-12 14:11:12 -0500

    api save data update

--- server/__init__.py
+++ server/__init__.py
@@ -5,22 +5,30 @@ from sqlalchemy import create_engine
 from flask_marshmallow import Marshmallow
 from image_filter import Image_Filter
 import os
-from .extensions import db
+# from server.extensions import db
+from server.views import *
 
 
 def create_app():
     app = Flask(__name__)
 
     # database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
     # init db
     db.init_app(app)
 
-    engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-
+    # image filter
     app.image_filter = Image_Filter()
+    # load saved content
+    saved_content = unpack_json_dicts(get_from_file(save_path))
+    if saved_content is not None:
+        app.image_filter.image_path_to_labels_dict, \
+        app.image_filter.album_to_image_paths_dict, \
+        app.image_filter.keyword_to_image_paths_dict = unpack_json_dicts(get_from_file(save_path))
 
+    # blueprint
     from server.views import main
     app.register_blueprint(main)
 
--- server/views.py
+++ server/views.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify, Blueprint, current_app
-from .extensions import db
-from .model import Model
+from server.extensions import db
+# from server.model import Images_Db
 import json
-
+import os
 
 main = Blueprint('main', __name__)
+
+save_path = os.path.join("./data.json")
 
 
 @main.route('/')
@@ -20,7 +22,10 @@ def add_images():
         current_app.image_filter.add_image(image_url)
     print(current_app.image_filter.image_path_to_labels_dict)
     # save
-    # ...
+    contents = jsonify_dicts(current_app.image_filter.image_path_to_labels_dict,
+                             current_app.image_filter.album_to_image_paths_dict,
+                             current_app.image_filter.keyword_to_image_paths_dict)
+    save_to_file(contents, save_path)
     # return updated image list according to the filters
     album = data["album"]
     keywords = data["keywords"]
@@ -36,7 +41,10 @@ def delete_images():
         current_app.image_filter.remove_image(image_url)
     print(current_app.image_filter.image_path_to_labels_dict)
     # save
-    # ....
+    contents = jsonify_dicts(current_app.image_filter.image_path_to_labels_dict,
+                             current_app.image_filter.album_to_image_paths_dict,
+                             current_app.image_filter.keyword_to_image_paths_dict)
+    save_to_file(contents, save_path)
     # return updated image list according to the filters
     album = data["album"]
     keywords = data["keywords"]
@@ -53,20 +61,50 @@ def get_images():
     return jsonify({"condition": "image filtered", "updated_images": list_of_images}), 201
 
 
-def save_to_database(images_to_labels, albums_to_images, keywords_to_images):
-    column_names = ['images_to_labels', 'albums_to_images', 'keywords_to_images']
-    column_dicts = [images_to_labels, albums_to_images, keywords_to_images]
-    # clear old data
-    db.session.query(Model).delete()
-    db.session.commit()
-    # add new data
-    for i in range(len(column_names)):
-        dictionary = Model(name=column_names[i], dict=column_dicts[i])
-        db.session.add(dictionary)
-    db.session.commit()
+def save_to_database():
+    pass
 
 
 def get_dicts_from_database():
     pass
 
 
+def save_to_file(content, file_path):
+    """
+    save json to file
+    content: json
+    file_path: string
+    """
+    if not os.path.exists(file_path):
+        open(file_path, "a")
+    with open(file_path, "w") as f:
+        json.dump(content, f)
+    return 0
+
+
+def get_from_file(file_path):
+    """
+    get json from file
+    file_path: string
+    return: json, None if file doesn't exist
+    """
+    if not os.path.exists(file_path):
+        print(f"file {file_path} doesn't exist")
+        return None
+    else:
+        with open(file_path, "r") as f:
+            return json.loads(f.read())
+
+
+def jsonify_dicts(image_to_label, album_to_image, keyword_to_image):
+    return {"image_to_label": image_to_label,
+            "album_to_image": album_to_image,
+            "keyword_to_image": keyword_to_image}
+
+
+def unpack_json_dicts(json_content):
+    if json_content is not None:
+        return (json_content["image_to_label"],
+                json_content["album_to_image"],
+                json_content["keyword_to_image"])
+    return None
