c51c0d8c54dab55fad3bcce15a8e150fb9b5ff85
Allyn Bao <baoallyn@gmail.com>
2022-12-26 21:46:43 -0500

    react select & delete update for image gallery

--- react_front_end/src/components/ImageUploader.jsx
+++ react_front_end/src/components/ImageUploader.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import {Form} from "react-bootstrap";
 import "./ImageUploader.css"
-import ImageGallery from "./ImageGallery";
+import UploadImages from "./UploadImages";
 
 class ImageUploader extends React.Component {
 
     render() {
         return (
-            <div className="image-uploader">
-            <Form>
-                <h1>Upload Images</h1>
-                <input 
-                    type="file" 
-                    multiple
-                    onChange={(event) => this.props.handleImageUploadSelect(event)}
-                    accept="image/jepg"
-                />
-                <button 
-                    type="submit"
-                    onClick={this.props.handleImageUpload}
-                >Upload</button>
-            </Form>
-            <h1>Selected Images</h1>
-            <ImageGallery imageList={this.props.files}/>
+            <div className="container image-uploader">
+            <span className="card d-flex flex-column border border-dark rounded p-3" >
+                <h3>Upload Images
+                <small className="text-muted">  Currenty Supported: JPEG</small>
+                </h3>
+                <div className="d-flex flex-row justify-content-center align-items-center">
+                    <input 
+                        className="form-control"
+                        type="file" 
+                        multiple
+                        onChange={(event) => this.props.handleImageUploadSelect(event)}
+                        accept="image/jepg"
+                    />
+                    <button 
+                        className="btn btn-dark"
+                        type="submit"
+                        onClick={this.props.handleImageUpload}
+                    >Upload</button>
+                </div>
+            </span>
+            <h4 class="text-muted">{this.props.uploadMessage}</h4>
+            <UploadImages imageList={this.props.imageList}/>
             </div>
         );
     }
