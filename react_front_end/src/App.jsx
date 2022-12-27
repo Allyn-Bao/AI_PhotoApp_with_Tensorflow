ced7410118a0b3b65b709d89ceaa847c0467bcb8
Allyn Bao <baoallyn@gmail.com>
2022-12-26 21:45:54 -0500

    react select & delete update for image gallery

--- react_front_end/src/App.jsx
+++ react_front_end/src/App.jsx
@@ -3,7 +3,6 @@ import {setState, useState} from "react";
 import NavigationBar from "./components/NavigationBar";
 import ImageGallery from "./components/ImageGallery";
 import ImageUploader from "./components/ImageUploader";
-import UploadImageList from "./components/UploadImageList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
@@ -13,26 +12,47 @@ class App extends React.Component {
   SERVER_URL = "http://127.0.0.1:5000";
 
   state = {
-    allImages:[],
-    imageList: [],
-    searchText: "",
-    uploadImageURLs: [],
-    updated: false
+    allImages:[], // all images without filters, for quick reloading to all images
+    imageList: [],  // list of image URLs given the current filters applied
+    searchKeywords: "",  // current search keyword in the search bar
+    uploadImageURLs: [],  // list of image URLs going to be uploaded to the backend
+    uploadMessage: "", // message prompt for comforming successful upload
+    selectEnabled: false, // select control for delete / download images in Gallary, checkboxes appears when selectEnabled
+    selectedIndex: [], // image index in imageList if images are selected - keep track of if images are selected / should be unselected
+    selectedImages: [], // images in Gallery selected
+    imageGalleryMessage: "", // message prompt for search / select / delete
+    updated: false  // false when page is reload or image uploaded, a signal to update / fatch the imageList
+    
   }
-
+  
+  /*
+  Go to page / reload page and update content
+  */
+  // when Home button in navbar is pressed
   handleHomeClick = () => {
     // reset images to all, clear all keyword search
     this.setState({ imageList: this.state.allImages })
-    this.setState({ searchText: "" })
+    this.setState({ searchKeywords: "" })
+    this.setState({ imageGalleryMessage: "" })
+    // if comming from Add Photos: clear uploadImageURLs
+    this.setState({ uploadImageURLs: [] })
+    this.setState({ uploadMessage: "" })
+  }
+  // when album button in navbar is pressed
+  handleAlbumClick = () => {
   }
 
+  /*
+  Search bar functions
+  */
+  // When Search input updated ( user changed text in search bar )
   handleSearchInput = event => {
-    // when search input onChange: update state variable searchText with new input
+    // when search input onChange: update state variable searchKeyword with new input
     this.setState({
-      searchText: event.target.value
+      searchKeywords: event.target.value
     });
   }
-
+  // When "Search" button is clicked - get search result and update imageList
   handleSearch = (keyword) => {
     // update state variable image list according to album / keywords
     console.log("searching: ", keyword)
@@ -50,8 +70,9 @@ class App extends React.Component {
       .then((response) => response.json())
       .then((data) => {
         // response data
-        console.log(data)
+        console.log(data.condition)
         this.setState({ imageList: data.images })
+        this.setState({ imageGalleryMessage: data.condition })
       })
         .catch((error) => {
           console.error(error);
@@ -59,6 +80,79 @@ class App extends React.Component {
 
   }
 
+  /*
+  ImageGallery
+  */
+  // When select clicked
+  updateSelect = () => {
+    // toogle the state of selectEnabled between true and false
+    const currentState = this.state.selectEnabled;
+    this.setState({ selectEnabled: !currentState })
+    // if select toogled off - clear all selectedImages
+    if (currentState) {
+      this.setState({ selectedIndex: [] })
+      this.setState({ selectedImages: [] })
+    }
+}
+  // When Image selected
+  handleSelectGalleryImage = (index) => {
+    console.log(`Selected: index: ${index}`)
+    // selected image
+    const selectedImage = this.state.imageList[index];
+    // if index already in selected index - need to unselect - remove from list
+    if (this.state.selectedIndex.includes(index)) {
+      // new selectedIndex & selectedImages with current image removed
+      const updatedSelectedIndex = this.state.selectedIndex.filter(i => i != index);
+      const updatedSelectedImages = this.state.selectedImages.filter(image => image != selectedImage);
+      this.setState({ selectedIndex: updatedSelectedIndex })
+      this.setState({ selectedImages: updatedSelectedImages })
+      
+    } else {
+      // select image, add to lists
+      const updatedSelectedIndex = this.state.selectedIndex.concat(index);
+      const updatedSelectedImages = this.state.selectedImages.concat(selectedImage)
+      this.setState({ selectedIndex: updatedSelectedIndex })
+      this.setState({ selectedImages: updatedSelectedImages })
+    }
+  }
+  // handle delte images
+  handleDeleteImages = () => {
+    // check if selected Enabled
+    if (this.state.selectEnabled) {
+      console.log(`index to delete: ${this.state.selectedIndex}`)
+      console.log(`images to delete: ${this.state.selectedImages}`)
+      // send delete request back to server
+      fetch('http://127.0.0.1:5000/delete_images', {
+      method:'POST',
+      body: JSON.stringify({
+        deleteImages: this.state.selectedImages,
+        album: null,
+        keywords: [],
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // response data
+        console.log(data.condition)
+        this.setState({ imageList: data.updated_images })
+        this.setState({ allImages: data.all_images })
+        this.setState({ imageGalleryMessage: data.condition })
+      })
+        .catch((error) => {
+          console.error(error);
+        });
+      // reset selected images lists
+      this.updateSelect();
+    }
+  }
+
+  /*
+  Upload images
+  */
+  // When user select file from local - update uploadImageURLs with URLs of selected file
   handleImageUploadSelect = (event) => {
     // when file uploader input onChange: update state variable uploadImageFiles with only image files
     const files = event.target.files;
@@ -77,10 +171,10 @@ class App extends React.Component {
       this.setState({ uploadImageURLs: imageURLs });
     });
   };
-
+  // When "Upload" button is clicked - upload image URLs to the backend and get updated imageList
   handleImageUpload = event => {
-    // stop auto refreshing
-    event.preventDefault();
+    // stop auto refreshing for debugging
+    // event.preventDefault();
     // send uploadImageURLs back to flask server
     fetch('http://127.0.0.1:5000/add_images', {
       method:'POST',
@@ -96,23 +190,26 @@ class App extends React.Component {
       .then((response) => response.json())
       .then((data) => {
         // response data
-        console.log(data)
-        this.setState({ imageList: data.updated_images })
-        this.setState({ allImages: data.all_images })
+        // console.log(data)
+        // this.setState({ imageList: data.updated_images })
+        // this.setState({ allImages: data.all_images })
+        console.log(data.condition)
+        this.setState({ uploadMessage: data.condition })
+
       })
         .catch((error) => {
           console.error(error);
         });
-    // window.location.reload();
-    // set reload home page
-    this.setState({ updated: false })
+    // set to reload
+    // this.setState({ updated: false })
   }
 
   render() {
     
+    // update displayed images when page is refreshed or new images uploaded
     if (this.state.updated == false) {
       // get image
-      fetch(`${this.SERVER_URL}/images`, {
+      fetch(`http://127.0.0.1:5000/images`, {
         method:'POST',
         body: JSON.stringify({
           album: null,
@@ -124,15 +221,16 @@ class App extends React.Component {
       })
         .then((response) => response.json())
         .then((data) => {
+          console.log(data.condition)
           this.state.allImages = data.images;
           this.state.imageList = data.images;
           this.setState({ updated: true });
+          this.setState({ imageGalleryMessage: "" })
         })
           .catch((error) => {
             console.error(error);
           }) 
     }
-    
 
     return (
       <div>
@@ -141,7 +239,7 @@ class App extends React.Component {
           handleRoute={this.handleRoute}
           handleSearchInput={this.handleSearchInput}
           handleSearch={this.handleSearch}
-          searchText={this.state.searchText}
+          searchKeywords={this.state.searchKeywords}
           handleHomeClick={this.handleHomeClick}
         />
         <Switch>
@@ -153,13 +251,19 @@ class App extends React.Component {
             <ImageUploader
               handleImageUploadSelect={this.handleImageUploadSelect}
               handleImageUpload={this.handleImageUpload}
-              files={this.state.uploadImageURLs}
+              imageList={this.state.uploadImageURLs}
+              uploadMessage={this.state.uploadMessage}
             />
             </div>
           </Route>
           <Route path="/">
             <ImageGallery
               imageList={this.state.imageList}
+              selectEnabled={this.state.selectEnabled}
+              updateSelect={this.updateSelect}
+              handleSelectGalleryImage={this.handleSelectGalleryImage}
+              handleDeleteImages={this.handleDeleteImages}
+              imageGalleryMessage={this.state.imageGalleryMessage}
             />
           </Route>
         </Switch>
--- react_front_end/src/components/ImageGallery.jsx
+++ react_front_end/src/components/ImageGallery.jsx
@@ -5,37 +5,36 @@ import "./ImageGallery.css";
 
 class ImageGallery extends React.Component {
 
-    state = {
-        selectEnabled: false,
-        selectButtonText: " Select "
-    }
-
-    updateSelect = () => {
-        const currentState = this.state.selectEnabled;
-        this.setState({ selectEnabled: !currentState })
-        this.setState({ selectButtonText: currentState ? "Unselect": " Select " })
-    }
-
     render() {
 
         const imageStyle = {
-            width: '100%',
-            height: '100%',
             objectFit: "cover",
             overflow: "hidden",
         };
 
         return (
             <div>
-                <div className="btn-group edit-buttons" role="group" aria-label="Edit Buttons">
-                    <button type="button" className="btn btn-secondary" onClick={this.updateSelect}>{this.state.selectButtonText}</button>
-                    <button type="button" className="btn btn-secondary">Delete</button>
+                {/* <h4 className="text-muted">{this.props.imageGalleryMessage}</h4> */}
+                <div className="btn-group edit-buttons " role="group" aria-label="Edit Buttons">
+                    <button type="button" className="btn btn-secondary opacity-80 bg-dark border-dark" onClick={this.props.updateSelect}>{this.props.selectEnabled ? "Unselect": "Select"}</button>
+                    <button type="button" className="btn btn-secondary opacity-80 bg-dark border-dark" onClick={this.props.handleDeleteImages}>Delete</button>
                 </div>
+                
                 <div className="image-gallery">
                     {this.props.imageList.map((imageURL, index) => {
-                        return <img 
-                            className="rounded mx-auto d-block"
-                            key={index} src={imageURL} loading="lazy" style={imageStyle} alt="image"/>;
+                        return (
+                            <div key={index} className="image-container">
+                                {
+                                    this.props.selectEnabled &&  <input className="form-check-input check-select" 
+                                                                        type="checkbox" value="" 
+                                                                        id="flexCheckDefault" 
+                                                                        onChange={() => this.props.handleSelectGalleryImage(index)}></input>
+                                }
+                                 <img 
+                                    className="rounded mx-auto d-block image"
+                                    key={index} src={imageURL} loading="lazy" style={imageStyle} alt="image"/>
+                            </div>
+                        )
                     })}
                 </div>
             </div>
